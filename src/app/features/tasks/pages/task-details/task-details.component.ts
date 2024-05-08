5de0a875197aae3db0c9e866548aa4ffac8cbdd7
AdongoJr2 <dev.mosesadongo@gmail.com>
2024-05-08 17:04:54 +0300

    Enable retrieving route information in task details page

--- src/app/features/tasks/pages/task-details/task-details.component.ts
+++ src/app/features/tasks/pages/task-details/task-details.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -13,4 +13,9 @@ import { RouterLink } from '@angular/router';
   styleUrl: './task-details.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TaskDetailsComponent { }
+export class TaskDetailsComponent {
+  @Input()
+  set taskId(taskId: string) {
+    console.log(taskId);
+  }
+}

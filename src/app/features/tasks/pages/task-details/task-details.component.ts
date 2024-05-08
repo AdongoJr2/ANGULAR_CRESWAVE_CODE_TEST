952e03a7c78331c2a13a4668dc055b11f0c07241
AdongoJr2 <dev.mosesadongo@gmail.com>
2024-05-08 21:52:56 +0300

    Create a simple Task Details/Edit UI

--- src/app/features/tasks/pages/task-details/task-details.component.ts
+++ src/app/features/tasks/pages/task-details/task-details.component.ts
@@ -1,21 +1,81 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, Input } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { TaskItem, TaskItemStatus } from '../task-list/task-list.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-task-details',
   standalone: true,
   imports: [
     CommonModule,
-    RouterLink,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
   ],
   templateUrl: './task-details.component.html',
   styleUrl: './task-details.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TaskDetailsComponent {
+  readonly TaskItemStatus = TaskItemStatus;
+  taskItemStatusValues = Object.values(TaskItemStatus)
+
+  // TODO: retrieve from API (service)
+  taskItem: TaskItem = {
+    id: +this.taskId,
+    title: 'Sample title',
+    description: 'Sample description',
+    status: TaskItemStatus.complete
+  };
+
   @Input()
   set taskId(taskId: string) {
     console.log(taskId);
+
+    this.updateFormFields(this.taskItem);
   }
+
+  taskForm = this.formBuilder.group({
+    title: ['', [Validators.required, Validators.minLength(3)]],
+    description: ['', [Validators.required, Validators.minLength(5)]],
+    status: this.formBuilder.control<TaskItemStatus | null>(null, { validators: [Validators.required] }),
+  });
+
+  constructor(
+    private readonly formBuilder: FormBuilder,
+  ) { }
+
+  get title() {
+    return this.taskForm.controls.title;
+  }
+
+  get description() {
+    return this.taskForm.controls.description;
+  }
+
+  get status() {
+    return this.taskForm.controls.status;
+  }
+
+  private updateFormFields(taskItem: TaskItem) {
+    this.taskForm.patchValue({
+      title: taskItem.title,
+      description: taskItem.description,
+      status: taskItem.status,
+    });
+  }
+
+  onSubmit() {
+    if (this.taskForm.invalid) return;
+
+    const formValue = this.taskForm.value;
+
+    console.log(formValue);
+  }
+
 }
--- src/app/features/tasks/pages/task-list/task-list.component.ts
+++ src/app/features/tasks/pages/task-list/task-list.component.ts
@@ -4,9 +4,12 @@ import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
-export type TaskItemStatus = 'complete' | 'incomplete';
+export enum TaskItemStatus {
+  'complete' = 'complete', 
+  'incomplete' = 'incomplete',
+}
 
 export interface TaskItem {
   id: number;
@@ -20,37 +23,37 @@ const taskList: Array<TaskItem> = [
     id: 1,
     title: 'Task 1',
     description: 'Task 1\'s description goes here',
-    status: 'incomplete',
+    status: TaskItemStatus.incomplete,
   },
   {
     id: 2,
     title: 'Task 2',
     description: 'Task 2\'s description goes here',
-    status: 'incomplete',
+    status: TaskItemStatus.incomplete,
   },
   {
     id: 3,
     title: 'Task 3',
     description: 'Task 3\'s description goes here',
-    status: 'complete',
+    status: TaskItemStatus.complete,
   },
   {
     id: 4,
     title: 'Task 4',
     description: 'Task 4\'s description goes here',
-    status: 'complete',
+    status: TaskItemStatus.complete,
   },
   {
     id: 5,
     title: 'Task 5',
     description: 'Task 5\'s description goes here',
-    status: 'incomplete',
+    status: TaskItemStatus.incomplete,
   },
   {
     id: 6,
     title: 'Task 6',
     description: 'Task 6\'s description goes here',
-    status: 'incomplete',
+    status: TaskItemStatus.incomplete,
   },
 ];
 
@@ -61,7 +64,6 @@ type TableColumnName = keyof TaskItem | 'actions';
   standalone: true,
   imports: [
     CommonModule,
-    RouterLink,
     MatTableModule,
     MatPaginatorModule,
     MatButtonModule,
@@ -76,16 +78,25 @@ export class TaskListComponent implements AfterViewInit {
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
+  constructor(
+    private readonly router: Router,
+    private readonly route: ActivatedRoute,
+  ) { }
+
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
   }
 
   handleTaskItemClick(taskItem: TaskItem) {
-    console.log(`Task Item Click ==== `, taskItem);
+    this.navigateToDetailsPage(taskItem.id);
   }
-
+  
   handleTaskEditBtnClick(taskItem: TaskItem) {
-    console.log(`Task Item [Edit] Click ==== `, taskItem);
+    this.navigateToDetailsPage(taskItem.id);
+  }
+  
+  private navigateToDetailsPage(taskItemId: number) {
+    this.router.navigate([`${taskItemId}/details`], { relativeTo: this.route });
   }
 
   handleTaskDeleteBtnClick(taskItem: TaskItem) {

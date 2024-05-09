0ac7eb4dd37e5a1a126de5202d7705631b127881
AdongoJr2 <dev.mosesadongo@gmail.com>
2024-05-09 23:38:16 +0300

    Implement retrieving Tasks from API

--- src/app/features/tasks/pages/task-list/task-list.component.ts
+++ src/app/features/tasks/pages/task-list/task-list.component.ts
@@ -8,45 +8,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { TasksService } from '../../services/tasks.service';
 import { TaskItem } from '../../models/task-item';
 import { TaskItemStatus } from '../../models/task-item-status';
-
-const taskList: Array<TaskItem> = [
-  {
-    id: 1,
-    title: 'Task 1',
-    description: 'Task 1\'s description goes here',
-    status: TaskItemStatus.INCOMPLETE,
-  },
-  {
-    id: 2,
-    title: 'Task 2',
-    description: 'Task 2\'s description goes here',
-    status: TaskItemStatus.INCOMPLETE,
-  },
-  {
-    id: 3,
-    title: 'Task 3',
-    description: 'Task 3\'s description goes here',
-    status: TaskItemStatus.COMPLETE,
-  },
-  {
-    id: 4,
-    title: 'Task 4',
-    description: 'Task 4\'s description goes here',
-    status: TaskItemStatus.COMPLETE,
-  },
-  {
-    id: 5,
-    title: 'Task 5',
-    description: 'Task 5\'s description goes here',
-    status: TaskItemStatus.INCOMPLETE,
-  },
-  {
-    id: 6,
-    title: 'Task 6',
-    description: 'Task 6\'s description goes here',
-    status: TaskItemStatus.INCOMPLETE,
-  },
-];
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 type TableColumnName = keyof TaskItem | 'actions';
 
@@ -65,7 +27,7 @@ type TableColumnName = keyof TaskItem | 'actions';
 })
 export class TaskListComponent implements AfterViewInit {
   displayedColumns: Array<TableColumnName> = ['id', 'title', 'description', 'status', 'actions'];
-  dataSource = new MatTableDataSource<TaskItem>(taskList);
+  dataSource = new MatTableDataSource<TaskItem>([]);
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
@@ -74,10 +36,23 @@ export class TaskListComponent implements AfterViewInit {
   constructor(
     private readonly router: Router,
     private readonly route: ActivatedRoute,
-  ) { }
+  ) {
+    this.retrieveTasks();
+  }
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
+  }
+
+  taskList: Array<TaskItem> = [];
+
+  private retrieveTasks() {
+    this.tasksService.getTasks()
+    .pipe(takeUntilDestroyed())
+    .subscribe(taskItems => {
+      this.taskList = taskItems;
+      this.dataSource = new MatTableDataSource<TaskItem>(this.taskList);
+    });
   }
 
   handleTaskItemClick(taskItem: TaskItem) {
